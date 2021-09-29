771136546fb12ea6cfbfdec1638b2367aa30e732
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-09-29 17:23:02 +0200

    Add files via upload
    First commit to main

---
+++ HistoryREST/HistoryREST/Controllers/CastlesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using HistoryREST.Entries;
+using HistoryREST.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HistoryREST.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CastlesController : ControllerBase
+    {
+        private readonly InMemCastlesRepository repository;
+        
+        public CastlesController()
+        {
+            repository = new InMemCastlesRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Castle> GetCastles()
+        {
+            var castles = repository.GetCastles();
+            return castles;
+
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Castle> GetCastle(Guid id)
+        {
+            var castle = repository.GetCastle(id);
+
+            if(castle is null)
+            {
+
+                return NotFound();
+            }
+
+            return castle;
+        }
+    }
+}
---
+++ HistoryREST/HistoryREST/Entries/castles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HistoryREST.Entries
+{
+    public record Castle
+    {
+        public Guid Id { get; init; }
+
+
+        public string Name { get; init; }
+
+        public string Location { get; init; }
+
+        public decimal Year { get; init; }
+        
+    }
+
+}
+
