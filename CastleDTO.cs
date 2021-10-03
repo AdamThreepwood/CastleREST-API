9079e8a629335cf6e2304b8dbad62d1ff2154bb7
AdamThreepwood <87753284+AdamThreepwood@users.noreply.github.com>
2021-10-03 18:49:30 +0200

    Add files via upload

---
+++ CastleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HistoryREST.DTOS
+{
+
+    public record CastleDto
+    { 
+    public Guid Id { get; init; }
+
+
+    public string Name { get; init; }
+
+    public string Location { get; init; }
+
+    public decimal Year { get; init; }
+
+    }
+
+}
+
---
+++ RegisterCastleDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HistoryREST.DTOS
+{
+    public record UpdateCastleDto
+    {
+        [Required]
+        public string Name { get; init; }
+        [Required]
+        public string Location { get; init; }
+        [Required]
+        [Range(1,2021)]
+        public decimal Year { get; init; }
+        }
+    }
+ 
